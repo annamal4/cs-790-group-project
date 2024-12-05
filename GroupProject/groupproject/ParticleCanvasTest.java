b025d23d52bed51c8c69476e4c466b9304c95aef
manvirkaur1555 <93561818+manvirkaur1555@users.noreply.github.com>
2024-12-04 19:56:30 -0600

    2 more tests..if in any case these test are not passing on your computers check the modules dependences...click on the project..properties..make sure the junit is an added library and that the junit.jupiter is listed as a dependency

---
+++ GroupProject/groupproject/ParticleCanvasTest.java
@@ -0,0 +1,34 @@
+package groupproject;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class ParticleCanvasTest {
+
+	
+    @Test
+    void testSetAndGetParticles() {
+        ParticleCanvas canvas = new ParticleCanvas(400);
+
+        // Create an array of particles
+        Particle[] particles = new Particle[5];
+        for (int i = 0; i < particles.length; i++) {
+            particles[i] = new Particle(100, 100);
+        }
+
+        // Set particles and verify
+        canvas.setParticles(particles);
+        assertArrayEquals(particles, canvas.getParticles(), "Particles do not match after setting and getting");
+    }
+
+    @Test
+    void testSetParticlesThrowsExceptionForNull() {
+        ParticleCanvas canvas = new ParticleCanvas(400);
+
+        // Setting null should throw the IllegalArgumentException
+        assertThrows(IllegalArgumentException.class, () -> canvas.setParticles(null), "Should throw exception for null input");
+    }
+
+    
+}
